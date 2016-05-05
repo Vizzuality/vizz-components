86d6713d925a944832f4066a4ad4ace8b34c5de8
David Inga <davidsingal@icloud.com>
2016-05-05 17:28:31 +0100

    removed uglify for compilation

--- webpack.config.js
+++ webpack.config.js
@@ -30,16 +30,7 @@ const config = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        dead_code: true,
-        drop_debugger: true,
-        drop_console: true
-      },
-      comments: false
-    })
+    new webpack.optimize.OccurrenceOrderPlugin()
   ]
 
 };
