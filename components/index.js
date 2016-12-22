45422e00a60369743bfedaaba63486caa4f80a20
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2016-12-22 15:56:32 +0100

    SvgIcon component

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 export default {};
 export { default as Globe } from './Globe';
 export { default as Spinner } from './Spinner';
-export { default as Modal} from './Modal';
+export { default as Modal } from './Modal';
+export { default as SvgIcon } from './SvgIcon';
--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,8 @@ import Globe from '../components/Globe';
 import Modal from '../components/Modal';
 import Spinner from '../components/Spinner';
 import RadioGroup from '../components/Form/RadioGroup';
-import CheckboxGroup from 'components/Form/CheckboxGroup';
+import CheckboxGroup from '../components/Form/CheckboxGroup';
+import SvgIcon from '../components/SvgIcon';
 
 
 storiesOf('UI Components', module)
@@ -18,7 +19,27 @@ storiesOf('UI Components', module)
 ))
 .add('Spinner', () => (
   <Spinner color="black" />
-));
+))
+.add('SvgIcon', () => {
+  const sprite = (
+    <svg style={{ position: 'absolute', overflow: 'hidden', height: 0, width: 0 }}>
+      <defs>
+        <symbol id="icon-eye" viewBox="0 0 24 24">
+          <title>eye</title>
+          <path className="path1" d="M12 4.5c-6.628 0-12 7.5-12 7.5s5.372 7.5 12 7.5 12-7.5 12-7.5-5.372-7.5-12-7.5zM12 16.5c-2.485 0-4.5-2.015-4.5-4.5s2.015-4.5 4.5-4.5 4.5 2.015 4.5 4.5-2.015 4.5-4.5 4.5zM12 9c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3z">
+          </path>
+        </symbol>
+      </defs>
+    </svg>
+  );
+
+  return (
+    <div>
+      {sprite}
+      <SvgIcon name="icon-eye" className="foo-bar" />
+    </div>
+  );
+});
 
 storiesOf('Form Components', module)
   .add('CheckboxGroup', () => {
